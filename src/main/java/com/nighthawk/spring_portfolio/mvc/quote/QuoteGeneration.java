325638045a69756b6baa685da06d20727d192fbc
vivianknee <vivianni452@gmail.com>
2023-10-03 13:08:59 -0700

    quote generation

--- src/main/java/com/nighthawk/spring_portfolio/mvc/quote/QuoteGeneration.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/quote/QuoteGeneration.java
@@ -1,5 +1,31 @@
 package com.nighthawk.spring_portfolio.mvc.quote;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
 public class QuoteGeneration {
-    
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            QuoteJpaRepository repository) {
+        return args -> {
+            Quote quote1 = new Quote(
+                "When life gives you lemons, make lemonade", 
+                "happy"
+            ); 
+
+            Quote quote2 = new Quote(
+                "Keep calm and carry on", 
+                "sad"
+            );
+
+            repository.saveAll(
+                    List.of(quote1, quote2)
+            );
+        };
+    }
 }
