7c64f23813dce54f56451bce9f4520b0e1704852
vivianknee <vivianni452@gmail.com>
2023-10-24 23:04:36 -0700

    wack indentation

--- src/main/java/com/nighthawk/spring_portfolio/mvc/quote/QuoteApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/quote/QuoteApiController.java
@@ -66,7 +66,7 @@ public class QuoteApiController {
         
         Quote savedQuote = repository.save(existingQuote);
         return new ResponseEntity<>(savedQuote, HttpStatus.OK);
-}
+    }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteQuote(@PathVariable Long id) {
@@ -78,6 +78,6 @@ public class QuoteApiController {
         
         repository.delete(existingQuote);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-}
+    }
 
 }
