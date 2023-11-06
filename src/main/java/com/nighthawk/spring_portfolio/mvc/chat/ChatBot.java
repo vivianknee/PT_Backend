6c85aba1231b210bc4878674d621ec7994ec4c6f
isabelle926 <gunawanisabelle@gmail.com>
2023-11-06 11:12:25 -0800

    chatbot with programAB

--- src/main/java/com/nighthawk/spring_portfolio/mvc/chat/ChatBot.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/chat/ChatBot.java
@@ -1,88 +1,65 @@
 package com.nighthawk.spring_portfolio.mvc.chat;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
-	
-/**
- * This class represents a simple chatbot that responds to user input based on a training data file.
- */
+import java.io.File;
+
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.History;
+import org.alicebot.ab.MagicBooleans;
+import org.alicebot.ab.MagicStrings;
+import org.alicebot.ab.utils.IOUtils;
+
 public class ChatBot {
-	private List<String> trainingData;
-	
-	/**
-	 * Constructor to initialize the chatbot with the training data from a file.
-	 *
-	 * @param trainingDataFile The file path of the training data file.
-	 * @throws IOException if there is an error reading the training data file.
-	 */
-	public ChatBot(String trainingDataFile) throws IOException {
-		trainingData = loadTrainingData(trainingDataFile);
-	}
-	
-	/**
-	 * Loads the training data from a file into a list of strings.
-	 *
-	 * @param trainingDataFile The file path of the training data file.
-	 * @return Returns a list of strings containing the training data.
-	 * @throws IOException if there is an error reading the training data file.
-	 */
-	private List<String> loadTrainingData(String trainingDataFile) throws IOException {
-		List<String> data = new ArrayList<>();
-		BufferedReader reader = new BufferedReader(new FileReader(trainingDataFile));
-		String line;
-		while ((line = reader.readLine()) != null) {
-			data.add(line);
-		}
-		reader.close();
-		return data;
-	}
-	
-	/**
-	 * Generates a random response from the training data.
-	 *
-	 * @return Returns a randomly selected response from the training data.
-	 */
-	private String generateResponse() {
-		Random random = new Random();
-		int index = random.nextInt(trainingData.size());
-		return trainingData.get(index);
-	}
-	
-	/**
-	 * Starts the chatbot and interacts with the user.
-	 */
-	public void startChat() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Chatbot: Hello! How can I assist you today?");
-		while (true) {
-			System.out.print("User: ");
-			String userInput = scanner.nextLine();
-			if (userInput.equalsIgnoreCase("exit")) {
-				System.out.println("Chatbot: Goodbye! Have a nice day!");
-				break;
-			}
-			String response = generateResponse();
-			System.out.println("Chatbot: " + response);
-		}
-		scanner.close();
-	}
-	
-	/**
-	 * Main method to run the chatbot.
-	 *
-	 * @param args The command line arguments.
-	 */
+	private static final boolean TRACE_MODE = false;
+	static String botName = "super";
+
 	public static void main(String[] args) {
 		try {
-			ChatBot chatbot = new ChatBot("traindata.txt");
-			chatbot.startChat();
-		} catch (IOException e) {
-			System.out.println("Error loading training data: " + e.getMessage());
+
+			String resourcesPath = getResourcesPath();
+			System.out.println(resourcesPath);
+			MagicBooleans.trace_mode = TRACE_MODE;
+			Bot bot = new Bot("super", resourcesPath);
+			Chat chatSession = new Chat(bot);
+			bot.brain.nodeStats();
+			String textLine = "";
+
+			while (true) {
+				System.out.print("Human : ");
+				textLine = IOUtils.readInputTextLine();
+				if ((textLine == null) || (textLine.length() < 1))
+					textLine = MagicStrings.null_input;
+				if (textLine.equals("q")) {
+					System.exit(0);
+				} else if (textLine.equals("wq")) {
+					bot.writeQuit();
+					System.exit(0);
+				} else {
+					String request = textLine;
+					if (MagicBooleans.trace_mode)
+						System.out.println(
+								"STATE=" + request + ":THAT=" + ((History) chatSession.thatHistory.get(0)).get(0)
+										+ ":TOPIC=" + chatSession.predicates.get("topic"));
+					String response = chatSession.multisentenceRespond(request);
+					while (response.contains("&lt;"))
+						response = response.replace("&lt;", "<");
+					while (response.contains("&gt;"))
+						response = response.replace("&gt;", ">");
+					System.out.println("Robot : " + response);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
+
+	private static String getResourcesPath() {
+		File currDir = new File(".");
+		String path = currDir.getAbsolutePath();
+		path = path.substring(0, path.length() - 2);
+		System.out.println(path);
+		String resourcesPath = path + File.separator + "src" + File.separator + "main" + File.separator + "resources";
+		return resourcesPath;
+	}
+
 }
